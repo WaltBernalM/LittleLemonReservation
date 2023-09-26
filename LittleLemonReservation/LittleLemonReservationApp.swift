b246ecd6fb1e3c45bd7cd9e21bda9bebc7a912be
Walter Bernal <wltrbm@gmail.com>
2023-09-25 17:52:09 -0700

    Initial Commit

---
+++ LittleLemonReservation/LittleLemonReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonReservationApp.swift
+//  LittleLemonReservation
+//
+//  Created by Walter Bernal Montero on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
